b0d6e2a70f9b61c17ff2228b5677559f02209d7b
sam <samantha.castillo.g@gmail.com>
2023-06-17 23:12:53 -0600

    Patch_Posts

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { list, remove } = require ("../usecases/post.usecase");
+const { list, remove, update } = require ("../usecases/post.usecase");
 const createError = require("http-errors")
 const router= express.Router();
 
 //GET POSTS
 
-router.get ("/", async (req, res) => {
+router.get("/", async (req, res) => {
     try{
         const posts=await list();
         res.json({
@@ -39,6 +39,27 @@ router.delete("/:id", async (req, res) => {
     });
   }
 });
+
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const updatedPost = await update(req.params.id, req.body);
+    if (!updatedPost) {
+      throw createError(404, "The id was non existant");
+    }
+    res.json({
+      success: true,
+      message: updatedPost,
+    });
+  } catch (err) {
+    res.status(err.status || 500);
+    res.json({
+      success: false,
+      message: "Post not found",
+    });
+  }
+});
+
 
 
 
--- src/usecases/post.usecase.js
+++ src/usecases/post.usecase.js
@@ -1,4 +1,5 @@
 
+const { publicDecrypt } = require("crypto");
 const Post = require("../models/post.model");
 
 //GET POSTS
@@ -9,7 +10,28 @@ const list = () => {
 
 const remove =(id) =>{
     const post = Post.findByIdAndDelete(id)
-    return post
+    return post;
 }
 
-module.exports = { list, remove }
+
+
+// PATCH POST /post/:id (protegido, solo usuarios con token pueden hacer esto)
+
+const update = async (id, data) => {
+    const post = await Post.findById(id);
+    if (data?.hashtags){
+    let hashtagsIndex= Object.keys(post.hashtags);
+    hashtagsIndex.forEach(hashtag => {
+        data.hashtags[hashtag] ? post.hashtags[hashtag] = data.hashtags[hashtag] : 
+        data.hashtags[hashtag]= post.hashtags [hashtag]
+        })
+    }
+    const postUpdate= await Post.findByIdAndUpdate(id, data, {returnDocument:"after"}); 
+    console.log(data)
+    return postUpdate;
+
+} 
+
+
+
+module.exports = { list, remove, update }
